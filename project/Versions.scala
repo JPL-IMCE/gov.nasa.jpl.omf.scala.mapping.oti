2911d9fdc3b1f944d5ac8235d7ccbbeed21d2b11
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-04 21:07:58 -0700

    MBEE-209:  JPL MBEE SBT Plugin build simplification

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,24 +1,13 @@
 
-import sbt.Process
-
 object Versions {
-  val scala = "2.11.6"
-  
-  // JPL MBEE release
-  val jpl_mbee_release_prefix="1800.02-"
-
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core = jpl_mbee_release_prefix+"e8ef32c064d457923beecfce50a4359a33c1aa22"
-
-  // OTI Trees version
-    
-  val oti_trees_prefix = "0.14.0"
-  val oti_trees_suffix = "770"
-  val oti_trees_version = oti_trees_prefix+"-"+oti_trees_suffix
+  val jpl_omf_core_revision="4fd1bd01a1258d6e901e62a318a784a4d202ffff"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="60d5d55571aeff7e4fb884ecd8d4e02b0da27ce6"
-  val jpl_mbee_core = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
-  val jpl_mbee_other = jpl_mbee_release_prefix+jpl_mbee_common_scala_libraries_revision
+  val jpl_mbee_common_scala_libraries_revision="fd6ef49108d43fad925a30b7a6dcc1d81b4b348d"
+
+  // OTI Trees version
+  val oti_trees_prefix = "0.14.0"
+  val oti_trees_suffix = "790"
 
 }
