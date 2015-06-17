30ce9a0f1b194831cae042a6ab4345d7636157a6
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-17 14:21:44 -0700

    IMCE-656: OTI/trees: 915 (TIWG-25 update) JPL MBEE Common Libs (+jena): 51a18484a374516f143e49fc04501cf5841dcb48 OMF/Core (+jena): 3768f28d7f5db0d5de99d98426fca10599e8049c

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,13 @@
 
 object Versions {
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core_revision="15c6b3eab6d162c11e1ac9b865a2579b1b0f8551"
+  val jpl_omf_core_revision="3768f28d7f5db0d5de99d98426fca10599e8049c"
 
   // JPL MBEE Common Scala Libraries
-  val jpl_mbee_common_scala_libraries_revision="576ddfdcdb17e9078f75a9ec8eef1336b82c5954"
+  val jpl_mbee_common_scala_libraries_revision="51a18484a374516f143e49fc04501cf5841dcb48"
 
   // OTI Trees version
   val oti_trees_prefix = "0.14.0"
-  val oti_trees_suffix = "910"
+  val oti_trees_suffix = "915"
 
 }
