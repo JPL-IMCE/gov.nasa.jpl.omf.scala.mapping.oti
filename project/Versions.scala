12d85f2d4964b4abed7b8236ebf411408407082a
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-09-27 23:12:42 -0700

    dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 
 object Versions {
   // JPL Ontology Modeling Framework Core
-  val jpl_omf_core_revision="6e41d537d694d8946846edff8378c53be4829950"
+  val jpl_omf_core_revision="3de1b3225eba925b94ca5b6c0438a39726dd470d"
 
   // JPL MBEE Common Scala Libraries
   val jpl_mbee_common_scala_libraries_revision="5e73616ed22dd4b6fe935b1ddd4a9bf28e0a8dd4"
@@ -11,11 +11,11 @@ object Versions {
   // OTI Trees version
 
   val oti_trees_prefix = version_prefix
-  val oti_trees_suffix = "444909"
+  val oti_trees_suffix = "444922"
 
   // OTI Canonical XMI version
 
   val oti_canonical_xmi_prefix = version_prefix
-  val oti_canonical_xmi_suffix = "444910"
+  val oti_canonical_xmi_suffix = "444928"
 
 }
